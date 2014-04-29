65e2fbddc2a5b80dd6341513f537d06c517cc996
Phil Bayfield <phil@bayfmail.com>
2014-04-29 14:12:33 +0100

    test iterating keys

--- Modulefile
+++ Modulefile
@@ -9,3 +9,4 @@ project_page ''
 
 ## Add dependencies, if any:
 # dependency 'username/name', '>= 1.2.0'
+dependency 'puppetlabs/stdlib', '4.1.0'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,29 @@
 # Define: useradd
 #
-# A puppet module for adding users - Extends existing functions to perform tasks such as adding SSH keys in a simplified way.
-define useradd {
+# A puppet module for adding users
+#
+# Extends existing functions to perform tasks such as adding SSH keys in a
+# simplified way.
+define useradd (
+  $name = $title,
+  $comment = '',
+  $shell = '/bin/bash',
+  $create_home = true,
+  $keys = []
+) {
+  each($keys) |$key| {
+    if is_hash($key) {
+      if !has_key($key, 'type') {
+        $key['type'] = 'ssh-rsa'
+      }
 
+      if !has_key($key, 'name') {
+        $key['name'] = "$name@puppet"
+      }
+
+      notice($key['key'])
+      notice($key['name'])
+      notice($key['type'])
+    }
+  }
 }
