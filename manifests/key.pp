2ab2e200fa80e47282bf61bf255b5cecb3204cd6
Phil Bayfield <phil@bayfmail.com>
2014-04-29 15:23:48 +0100

    debugging

--- manifests/key.pp
+++ manifests/key.pp
@@ -1,9 +1,12 @@
 define useradd::key {
   notify {"In useradd::key.pp":}
   if is_hash($name) {
+    # If type isn't set default to RSA
     if !has_key($name, 'type') {
       $name['type'] = 'ssh-rsa'
     }
+    
+    # If key name isn't set generate one
     if !has_key($name, 'name') {
       $name['name'] = "$name[name]@puppet"
     }
